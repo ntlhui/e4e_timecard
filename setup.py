cb5af95e0d795a5aacff5f49cfeff34ce5fe2878
Nathan Hui <ntlhui@gmail.com>
2022-01-30 20:53:14 -0800

    Added setup.py, fixed entry point

---
+++ setup.py
@@ -0,0 +1,20 @@
+from importlib_metadata import entry_points
+from setuptools import setup, find_packages
+import timecard
+setup(
+    name='E4ETimecard',
+    version=timecard.__version__,
+    description="E4E Timecard Application",
+    author='Nathan Hui',
+    author_email='nthui@eng.ucsd.edu',
+    packages=find_packages(),
+    install_requires=[
+        'pyrebase',
+        'ipython'
+    ],
+    entry_points={
+        'console_scripts': [
+            'timecard=e4e_timecard:main'
+        ]
+    }
+)
